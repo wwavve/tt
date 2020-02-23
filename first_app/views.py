efe97a9a143ee98c224400487a8c8eff38e5fc51
nick <wwavve@yandex.ru>
2020-02-23 20:12:01 +0300

    add templates

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<em>Hello World!</em>')
+    my_dict = {'insert_me': 'Now I\'m coming from first_app/index.html'}
+    return render(request, 'first_app/index.html', context=my_dict)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from first_app import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('first_app/', include('first_app.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
