458f9394b218428924c9b528405fb5d7412dc11e
nick <wwavve@yandex.ru>
2020-02-24 08:41:16 +0300

    fix populating script

--- populate_first_app.py
+++ populate_first_app.py
@@ -1,9 +1,13 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+import django
+django.setup()
 import random
-from first_app.models import AccessRecord, Topic, WebPage
+from first_app.models import AccessRecord, WebPage, Topic
 from faker import Faker
 
-fake = Faker()
+fakegen = Faker()
 topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
 
 
@@ -17,30 +21,23 @@ def populate(n=5):
     for entry in range(n):
         top = add_topic()
 
-        fake_url = fake.url()
-        fake_date = fake.date()
-        fake_name = fake.company()
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
 
-        web = WebPage.objects.get_or_create(
+        webpg = WebPage.objects.get_or_create(
             topic=top,
-            url=fake_url,
             name=fake_name,
+            url=fake_url,
         )[0]
 
-        acc_rec = AccessRecord.objects.get_or_create(
-            name=web,
+        access = AccessRecord.objects.get_or_create(
+            name=webpg,
             date=fake_date,
         )[0]
 
 
-# TODO find out why is not working
 if __name__ == '__main__':
     print('populating script!')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tangle.settings')
-
-    import django
-
-    django.setup()
-
     populate(20)
     print('populating complete!')
